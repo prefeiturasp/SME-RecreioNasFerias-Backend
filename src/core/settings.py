f1723bc915234d3979165148e26a956fb759e65b
Leonardo Rodrigues <59567995+leorjr@users.noreply.github.com>
2026-04-28 10:27:38 -0300

    feat: implementa dockerização da aplicação e conexão com banco de dados local

--- src/core/settings.py
+++ src/core/settings.py
@@ -72,10 +72,19 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
